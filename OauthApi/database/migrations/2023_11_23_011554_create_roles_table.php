a087b16fec2aed040e1a3ef617caa9289cdd8a64
Mateo Sosa <mateesosar@gmail.com>
2023-11-27 22:59:44 -0300

    Implementacion de CI/CD

---
+++ OauthApi/database/migrations/2023_11_23_011554_create_roles_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('rol')->unique();
+            $table->timestamps();
+        });
+
+        DB::table('roles')->insert([
+            ['rol' => 'admin'],
+            ['rol' => 'normal'],
+        ]);
+
+       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
